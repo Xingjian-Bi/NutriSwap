e67de1379f5028a28a416f01c7e06ad97b9a6792
Xingjian-Bi <harry1672869@gmail.com>
2024-11-05 22:36:50 -0800

    Fix invalid middleware function reference and add CORS

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
@@ -14,10 +15,11 @@ const PORT = process.env.PORT || 5050;
 
 const __dirname = path.resolve();
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // allows us to accept JSON data in the req.body
 app.use(express.json());
 // allows us to parse cookies
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
