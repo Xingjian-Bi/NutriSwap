59f44566dcf68f322ae86d18efa2c4ac947b43a5
kkkkevx <xuk14@miamioh.edu>
2024-11-12 17:00:32 -0800

    fix bugs

--- backend/routes/foodrecommendation.route.js
+++ backend/routes/foodrecommendation.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import postRecommend from '../controllers/product.controller.js'; 
+import postRecommend from '../controllers/foodrecommendation.controller.js'; 
 
 const router = express.Router();
 
-router.post('/recommend', postRecommend)
+router.post('/recommend', postRecommend);
+
+export default router;
