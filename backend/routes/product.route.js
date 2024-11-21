0efa59c78efc8dd5976e1b3161ba8c9fa9fae4bd
Edward Burke <burke.e@northeastern.edu>
2024-11-21 11:11:13 -0800

    Added favorites page

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import {User} from "../models/user.model.js";
 
 import {
     createProduct,
@@ -17,5 +18,22 @@ router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
 router.post("/favorites/add", addFavorite);
 router.post("/favorites/remove", removeFavorite);
+
+router.get("/:userId/favorites", async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const user = await User.findById(userId).populate("favorites");
+        console.log("Populated user:", user);
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+        res.status(200).json({ success: true, favorites: user.favorites });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+});
+
 
 export default router;
